723f71328faa777dd967ed8845f5416aa8363ae6
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-01-10 17:14:16 +0500

    10-01-2023

---
+++ 10-01-2023/app.js
@@ -0,0 +1,23 @@
+// for(let i=0;i<10;i+5){
+//     document.writeln(i+"<br>")
+// }
+// let sum=1
+// for(let j=1;j<=10;j++){
+//   sum=sum+j;
+// }
+
+// document.write(sum)
+// let value=0
+// for(let i=1;i<=20;i++){
+//     value=value*i
+// }
+// document.write(value)
+
+
+for(let i=1;i<=20;i++){
+    if(i%2===0){
+        
+        continue
+    }
+    document.write(i+"<br>")
+}
